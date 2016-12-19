ffbf5939fa1e93aa40511ba6d7298898968aeb91
Z-Hamza <hamzazoltan@citromail.hu>
2016-12-19 16:44:15 +0100

    workshop done

--- week09/Day01/main/java/com/greenfoxacademy/springstart/Controllers/HelloWebcontroller.java
+++ week09/Day01/main/java/com/greenfoxacademy/springstart/Controllers/HelloWebcontroller.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -18,18 +19,25 @@ public class HelloWebcontroller {
             "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
             "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
 
+    String[] colors = {"red", "blue", "black", "green", "yellow", "orange", "darkred", "pink", "darkblue", "darkgreen"};
+
     @RequestMapping("/web/greeting")
     public String greeting(@RequestParam() String name , Model model) {
-
+        Random rnd = new Random();
         long counter = atomiccounter.incrementAndGet();
         model.addAttribute("name", name);
         model.addAttribute("counter", counter);
+        int randomH = rnd.nextInt(hellos.length-1);
 
-        String hellosnumber = "hello";
+        model.addAttribute("hello", hellos[randomH]);
+        int randomC = rnd.nextInt(colors.length-1);
+        model.addAttribute("rndcolor", colors[randomC]);
+        int randomsize = rnd.nextInt(40) + 16;
+        model.addAttribute("rndsize", randomsize);
 
-        for(int i = 1; i < hellos.length; i++){
-            model.addAttribute()
-        }
+
+
+
         return "greeting";
     }
 }
