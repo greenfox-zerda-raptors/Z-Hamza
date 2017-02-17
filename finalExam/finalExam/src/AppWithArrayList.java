c0d1d2e76f68f0acdf1a54747a62f16c2f4f6805
Z-Hamza <hamzazoltan@citromail.hu>
2017-02-17 09:37:00 +0100

    fixed issues, added psvm for testing

--- finalExam/finalExam/src/AppWithArrayList.java
+++ finalExam/finalExam/src/AppWithArrayList.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Zoltán on 2017.02.17..
@@ -10,19 +11,26 @@ import java.util.ArrayList;
 public class AppWithArrayList {
 
     public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> testList = new ArrayList<>();
+        testList.add(new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7)));
+        testList.add(new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7)));
+        testList.add(new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7)));
+        testList.add(new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7)));
+        testList.add(new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7)));
+
+        System.out.println(sumNumbersInMatrix(testList));
 
     }
 
-    public int sumNumbersInMatrix(ArrayList<ArrayList<Integer>> inTakeList){
+    public static int sumNumbersInMatrix(ArrayList<ArrayList<Integer>> inTakeList){
         int result = 0;
         for(int i = 0; i < inTakeList.size(); i++){
             ArrayList<Integer> currentList = inTakeList.get(i);
             for(int j = 0; j < currentList.size(); j++){
-                int number = currentList.get(i);
-                result += number;
+                int number = currentList.get(j);
+                result = result + number;
             }
         }
-
         return result;
     }
 }
